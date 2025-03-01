041f8bd8fbd1066ab9df9d8b8070ab80f7d783a7
nickinn-ttw <drnickolascruz@gmail.com>
2025-03-01 00:54:38 -0300

    Isso é meu

---
+++ script.js
@@ -0,0 +1,7 @@
+function mudarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    const texto = document.getElementById("texto");
+    
+    mensagem.innerText = "Eu nunca fui tão feliz em te esperar nestes anos todos❤️";
+    texto.innerText = "Você é a mulher mais incrível que ja conheci, espero passar os restos dos meus dias com você!";
+}
